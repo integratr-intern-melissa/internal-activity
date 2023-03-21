00102f1b7e3499e4267c37760cff0a983ceb1848
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-03-21 11:32:07 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+index.php
+    <?php
+
+$course1 = {
+    'BSIT'
+    'BSBA'
+    'HMO'
+    'NC2'
+    'ENGINEERING'
+    'NURSE'
+    'ARCHITECT'
+    'TOURISM'
+
+    ;
+
+    $course2 = [
+        'HMO'
+        'ENGINEERING'
+        'NURSE'
+    ];
+}
+
+
+
+
