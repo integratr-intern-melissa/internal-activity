fd5ff70c6b5243a396a51f0b65e437699b27c8b6
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-03-28 11:36:18 +0800

    added deleting and update

---
+++ products/delete.php
@@ -0,0 +1,12 @@
+<?php 
+
+include_once("../connect.php");
+
+if (isset($_GET['id'])) {
+    $sql = "DELETE FROM products where id=" . $_GET['id'];
+
+    $query = $conn->query($sql) or die ($conn->error);
+
+    header("Location: ../product.php");
+    exit();
+}
